96bdad0716c4e11fd08b7d8d81a382e631e5f88c
Cristina Castro <cccastro2@myseneca.ca>
2024-02-03 14:37:32 +0000

    clean up code in network.tf and output.tf

--- output.tf
+++ output.tf
@@ -6,9 +6,8 @@ output "public_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
 
-output "dockerhost-ip" {
-  value = aws_instance.dockerhost.public_ip
-
+output "dockerhost-public-ip" {
+  value = aws_eip.static_eip.public_ip
 }
 
 output "aws_ecr_repository" {
